bede66522bb4a83bc59b235f91f032383a4e3292
Pierre Carrier <pc@rrier.ca>
2021-11-28 00:39:38 -0500

    smaller radius, tone the blue down

--- src/sizing.ts
+++ src/sizing.ts
@@ -1,7 +1,7 @@
 export const LARGE_CROWN_RADIUS = Math.sqrt(2) / 4;
 export const LAST_ACTION_RADIUS = LARGE_CROWN_RADIUS - 0.02;
 export const PIECE_RADIUS = LARGE_CROWN_RADIUS - 0.05;
-export const SLOT_RADIUS = 0.1;
+export const SLOT_RADIUS = 0.075;
 export const CROWN_RADIUS = 0.15;
 export const LINE_MARGIN = 0.2;
 export const LINE_MARGIN_DIAGONAL = LINE_MARGIN / Math.sqrt(2);
