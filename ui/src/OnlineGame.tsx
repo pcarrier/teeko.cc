d1f4822e41f3f489e554ded82857275df92a7458
Pierre Carrier <pc@rrier.ca>
2021-12-11 15:28:56 -0500

    h1

--- ui/src/OnlineGame.tsx
+++ ui/src/OnlineGame.tsx
@@ -3,5 +3,10 @@ import { FunctionComponent } from "preact";
 export const OnlineGame: FunctionComponent<{
   room: string;
 }> = ({ room }: { room: string }) => {
-  return <p>Online is not supported yet.</p>;
+  return (
+    <>
+      <h1>{room}</h1>
+      <p>Online is not supported yet.</p>
+    </>
+  );
 };
