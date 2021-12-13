207e91520bf78403a2aee95994f34caef279f378
Pierre Carrier <pc@rrier.ca>
2021-12-13 13:39:33 -0500

    autoUpdate

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,11 +6,12 @@ export default defineConfig({
   plugins: [
     preact(),
     VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.svg", "robots.txt"],
       manifest: {
         name: "Teeko by John Scarne",
         short_name: "Teeko",
         theme_color: "#000000",
-        includeAssets: ["icon.svg", "robots.txt"],
         icons: [
           {
             src: "/icon-192x192.png",
