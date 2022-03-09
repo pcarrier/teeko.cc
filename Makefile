633435d616b5c340df027c2ab27a864f4376c2bf
Pierre Carrier <pc@rrier.ca>
2022-03-08 19:52:36 -0500

    ws + upgrades

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 .PHONY: deploy
-deploy: deploy-ui
+deploy: deploy-ui deploy-ws
 
 ui/public/icon-512x512.png: ui/public/icon.svg
 	rsvg-convert --height 512 --output ui/public/icon-512x512.png ui/public/icon.svg
@@ -13,8 +13,13 @@ build-ui: ui/public/icon-192x192.png ui/public/icon-512x512.png
 
 .PHONY: deploy-ui
 deploy-ui: build-ui
-	rsync --archive --delay-updates ui/dist/ ident.me:/data/teeko.cc/
+	rsync --archive --delay-updates ui/dist/ horse:/data/teeko.cc/
 	curl -sfX POST "https://api.cloudflare.com/client/v4/zones/664e4bfb647853cad92f1bf7d0a20b35/purge_cache" \
 		-H "Authorization: Bearer $$(< ~/.cfpurgetoken)" \
 		-H "Content-Type: application/json" \
 		--data '{"purge_everything":true}'
+
+.PHONY: deploy-ws
+deploy-ws:
+	rsync --archive ws/ horse:/data/ws.teeko.cc/
+	ssh horse doas systemctl restart teeko-ws
---
+++ ws/index.ts
@@ -0,0 +1,103 @@
+type RoomState = unknown;
+
+interface Room {
+    url: string;
+    clients: WebSocket[];
+    state: RoomState;
+}
+
+interface Server {
+    rooms: Map<string, Room>;
+}
+
+const serverState: Server = {
+    rooms: new Map(),
+};
+
+interface Context {
+    socket: WebSocket;
+    roomURL: string;
+}
+
+function getRoom(ctx: Context): Room {
+    const url = ctx.roomURL;
+    const existing = serverState.rooms.get(url);
+    if (existing) {
+        existing.clients.push(ctx.socket);
+        return existing;
+    }
+    const created: Room = {
+        url,
+        clients: [ctx.socket],
+        state: undefined,
+    };
+    serverState.rooms.set(url, created);
+    console.log(`Opened room ${url}`);
+    return created;
+}
+
+function sendState(room: Room, socket: WebSocket) {
+    socket.send(JSON.stringify({ state: room.state }));
+}
+
+function setState(room: Room, state: RoomState, from: WebSocket) {
+    room.state = state;
+    room.clients.forEach((c) => {
+        if (c !== from) sendState(room, c);
+    });
+}
+
+function closeRoom(room: Room) {
+    serverState.rooms.delete(room.url);
+    console.log(`Closed room ${room.url}`);
+}
+
+function handleConnected(ctx: Context) {
+    const room = getRoom(ctx);
+    sendState(room, ctx.socket);
+}
+
+function handleMessage(ctx: Context, data: string) {
+    const room = getRoom(ctx);
+    const msg = JSON.parse(data);
+    if (msg.state) {
+        setState(room, msg.state, ctx.socket);
+    }
+}
+
+function handleError(ctx: Context, evt: Event | ErrorEvent) {
+    console.log(`Error ${evt instanceof ErrorEvent ? evt.message : evt.type}`);
+}
+
+function handleClose(ctx: Context, evt: CloseEvent) {
+    const room = getRoom(ctx);
+    room.clients = room.clients.filter((c) => c !== ctx.socket);
+    if (room.clients.length === 0) {
+        closeRoom(room);
+    }
+}
+
+async function main() {
+    for await (const conn of Deno.listen({ port: 8081 })) {
+        const httpConn = Deno.serveHttp(conn);
+        for await (const evt of httpConn) {
+            if (evt.request.headers.get("upgrade") != "websocket") {
+                await evt.respondWith(new Response(null, { status: 501 }));
+            } else {
+                const roomURL = evt.request.url;
+                const { socket, response } = Deno.upgradeWebSocket(evt.request);
+                const ctx: Context = { socket, roomURL };
+                socket.onopen = () => handleConnected(ctx);
+                socket.onmessage = (m) => handleMessage(ctx, m.data);
+                socket.onclose = (e) => handleClose(ctx, e);
+                socket.onerror = (e) => handleError(ctx, e);
+                await evt.respondWith(response);
+            }
+        }
+    }
+}
+
+main().catch((e) => {
+    console.log(e);
+    Deno.exit(1);
+});
