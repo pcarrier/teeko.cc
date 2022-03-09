c5a7eb9f013555be10c50970863506608e7cf63e
Pierre Carrier <pc@rrier.ca>
2022-03-09 13:38:40 -0500

    ws: keep rooms around for a minute

--- ws/index.ts
+++ ws/index.ts
@@ -1,107 +1,111 @@
 type RoomState = unknown;
 
 interface Room {
-    url: string;
-    clients: WebSocket[];
-    state: RoomState;
+  url: string;
+  clients: WebSocket[];
+  state: RoomState;
+  timeout?: number;
 }
 
 interface Server {
-    rooms: Map<string, Room>;
+  rooms: Map<string, Room>;
 }
 
 const serverState: Server = {
-    rooms: new Map(),
+  rooms: new Map()
 };
 
 interface Context {
-    socket: WebSocket;
-    roomURL: string;
+  socket: WebSocket;
+  roomURL: string;
 }
 
 function getRoom(ctx: Context): Room {
-    const url = ctx.roomURL;
-    const existing = serverState.rooms.get(url);
-    if (existing) {
-        existing.clients.push(ctx.socket);
-        return existing;
-    }
-    const created: Room = {
-        url,
-        clients: [ctx.socket],
-        state: undefined,
-    };
-    serverState.rooms.set(url, created);
-    console.log(`Opened room ${url}`);
-    return created;
+  const url = ctx.roomURL;
+  const existing = serverState.rooms.get(url);
+  if (existing) {
+    existing.clients.push(ctx.socket);
+    return existing;
+  }
+  const created: Room = {
+    url,
+    clients: [ctx.socket],
+    state: undefined
+  };
+  serverState.rooms.set(url, created);
+  console.log(`Opened room ${url}`);
+  return created;
 }
 
 function sendState(room: Room, socket: WebSocket) {
-    socket.send(JSON.stringify({ state: room.state }));
+  socket.send(JSON.stringify({ state: room.state }));
 }
 
 function setState(room: Room, state: RoomState, from: WebSocket) {
-    room.state = state;
-    room.clients.forEach((c) => {
-        if (c !== from) sendState(room, c);
-    });
+  room.state = state;
+  room.clients.forEach((c) => {
+    if (c !== from) sendState(room, c);
+  });
 }
 
 function closeRoom(room: Room) {
-    serverState.rooms.delete(room.url);
-    console.log(`Closed room ${room.url}`);
+  serverState.rooms.delete(room.url);
+  console.log(`Closed room ${room.url}`);
 }
 
 function handleConnected(ctx: Context) {
-    const room = getRoom(ctx);
-    sendState(room, ctx.socket);
+  const room = getRoom(ctx);
+  if (room.timeout) {
+    clearTimeout(room.timeout);
+    room.timeout = undefined;
+  }
+  sendState(room, ctx.socket);
 }
 
 function handleMessage(ctx: Context, data: string) {
-    const room = getRoom(ctx);
-    try {
-        const msg = JSON.parse(data);
-        if (msg.state) {
-            setState(room, msg.state, ctx.socket);
-        }
-    } catch (e) {
-        console.log(`handleMessage error ${e}`);
+  const room = getRoom(ctx);
+  try {
+    const msg = JSON.parse(data);
+    if (msg.state) {
+      setState(room, msg.state, ctx.socket);
     }
+  } catch (e) {
+    console.log(`handleMessage error ${e}`);
+  }
 }
 
 function handleError(ctx: Context, evt: Event | ErrorEvent) {
-    console.log(`WS error ${evt instanceof ErrorEvent ? evt.message : evt.type}`);
+  console.log(`WS error ${evt instanceof ErrorEvent ? evt.message : evt.type}`);
 }
 
 function handleClose(ctx: Context, evt: CloseEvent) {
-    const room = getRoom(ctx);
-    room.clients = room.clients.filter((c) => c !== ctx.socket);
-    if (room.clients.length === 0) {
-        closeRoom(room);
-    }
+  const room = getRoom(ctx);
+  room.clients = room.clients.filter((c) => c !== ctx.socket);
+  if (room.clients.length === 0)
+    room.timeout = setTimeout(() => closeRoom(room), 10_000);
 }
 
 async function main() {
-    for await (const conn of Deno.listen({ port: 8081 })) {
-        const httpConn = Deno.serveHttp(conn);
-        for await (const evt of httpConn) {
-            if (evt.request.headers.get("upgrade") != "websocket") {
-                await evt.respondWith(new Response(null, { status: 501 }));
-            } else {
-                const roomURL = evt.request.url;
-                const { socket, response } = Deno.upgradeWebSocket(evt.request);
-                const ctx: Context = { socket, roomURL };
-                socket.onopen = () => handleConnected(ctx);
-                socket.onmessage = (m) => handleMessage(ctx, m.data);
-                socket.onclose = (e) => handleClose(ctx, e);
-                socket.onerror = (e) => handleError(ctx, e);
-                await evt.respondWith(response);
-            }
-        }
+  for await (const conn of Deno.listen({ port: 8081 })) {
+    const httpConn = Deno.serveHttp(conn);
+    for await (const evt of httpConn) {
+      if (evt.request.headers.get("upgrade") != "websocket") {
+        await evt.respondWith(new Response(null, { status: 501 }));
+      } else {
+        const roomURL = evt.request.url;
+        const { socket, response } = Deno.upgradeWebSocket(evt.request);
+        const ctx: Context = { socket, roomURL };
+        socket.onopen = () => handleConnected(ctx);
+        socket.onmessage = (m) => handleMessage(ctx, m.data);
+        socket.onclose = (e) => handleClose(ctx, e);
+        socket.onerror = (e) => handleError(ctx, e);
+        await evt.respondWith(response);
+      }
     }
+  }
 }
 
 main().catch((e) => {
-    console.log(e);
-    Deno.exit(1);
+  console.log(e);
+  Deno.exit(1);
 });
