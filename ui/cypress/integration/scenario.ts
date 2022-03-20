e9c13a9379763b9e29ad26a1d6c335170bd76796
Pierre Carrier <pc@rrier.ca>
2022-03-19 23:47:17 -0400

    basic UI tests in cypress

---
+++ ui/cypress/integration/scenario.ts
@@ -0,0 +1,31 @@
+describe("My First Test", () => {
+  it("visits teeko.cc", () => {
+    cy.visit("http://127.0.0.1:3000");
+    cy.contains("Blue opens.");
+    cy.contains("Play").click();
+    cy.contains("Friends").click();
+    cy.contains("Cancel").click();
+    cy.contains("Help").click();
+    cy.contains("Play").click();
+    cy.contains("Friends").click();
+    cy.get("input").type("hello");
+    cy.contains("Join").click();
+    cy.contains("Invite").click();
+  });
+
+  it("changes language", () => {
+    cy.visit("http://127.0.0.1:3000");
+    cy.get("select").select("fr");
+    cy.contains("Jouer").click();
+    cy.contains("Aide").click();
+    cy.get("select").select("en");
+    cy.contains("Play");
+    // cy.contains("Play").click();
+    // cy.contains("Help").click();
+    // cy.contains("Play").click();
+    // cy.contains("Friends").click();
+    // cy.get("input").type("hello");
+    // cy.contains("Join").click();
+    // cy.contains("Invite").click();
+  });
+});
