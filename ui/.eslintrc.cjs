4f26a4891d61e02c12d3e7925dd5f6687958d090
Pierre Carrier <pc@rrier.ca>
2022-03-21 19:11:25 -0400

    prettier, nits

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ["@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    settings: {
-        "preact-i18n": {
-            languageFiles: [
-                {name: 'bg', path: 'src/translations/bg.json'},
-                {name: 'en', path: 'src/translations/en.json'},
-                {name: 'es', path: 'src/translations/es.json'},
-                {name: 'fr', path: 'src/translations/fr.json'},
-                {name: 'pt', path: 'src/translations/pt.json'},
-                {name: 'zh', path: 'src/translations/zh.json'},
-            ]
-        }
-    }
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: {
+    "preact-i18n": {
+      languageFiles: [
+        { name: "bg", path: "src/translations/bg.json" },
+        { name: "en", path: "src/translations/en.json" },
+        { name: "es", path: "src/translations/es.json" },
+        { name: "fr", path: "src/translations/fr.json" },
+        { name: "pt", path: "src/translations/pt.json" },
+        { name: "zh", path: "src/translations/zh.json" },
+      ],
+    },
+  },
 };
