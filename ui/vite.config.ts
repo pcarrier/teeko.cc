a33fc9a28bee4a018f1426287a4c2615347dab26
Pierre Carrier <pc@rrier.ca>
2022-03-24 19:01:33 -0400

    react->preact replacement

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -45,4 +45,12 @@ export default defineConfig({
     }),
   ],
   build: { sourcemap: true },
+  resolve: {
+    alias: [
+      { find: "react", replacement: "preact/compat" },
+      { find: "react-dom/test-utils", replacement: "preact/test-utils" },
+      { find: "react-dom", replacement: "preact/compat" },
+      { find: "react/jsx-runtime", replacement: "preact/jsx-runtime" },
+    ],
+  },
 });
