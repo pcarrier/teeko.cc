357721894545d590daaafcb2045ff35e1327330d
Pierre Carrier <pc@rrier.fr>
2022-10-26 16:16:38 -0400

    add esperanto in list

--- ui/src/translations.ts
+++ ui/src/translations.ts
@@ -1,5 +1,6 @@
 import bg from "./translations/bg.json";
 import en from "./translations/en.json";
+import eo from "./translations/eo.json";
 import es from "./translations/es.json";
 import fr from "./translations/fr.json";
 import pt from "./translations/pt.json";
@@ -8,6 +9,7 @@ import zh from "./translations/zh.json";
 export default {
   bg,
   en,
+  eo,
   es,
   fr,
   pt,
